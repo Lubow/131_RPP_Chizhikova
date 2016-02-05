33c63a5cfe65b414b451189138cf005361a08909
Lubow <lubow.sobaka@gmail.com>
2016-02-05 14:27:15 +0300

    +Вывод на экран сообщение

--- 1/1/Program.cs
+++ 1/1/Program.cs
@@ -14,12 +14,52 @@ namespace _1
             x = Convert.ToInt32(Console.ReadLine()); y = Convert.ToInt32(Console.ReadLine()); //2.	Ввод с клавиатуры их значений.
             int z = x + y; //Объявление переменной z, инициализировав ее суммой x и y.
             Console.WriteLine(z); // Вывод z на экран
-            //Вывод на экран сообщение о том какое z число: положительное или отрицательнлое, четное или нечетное, делиться ли оно на 5, 10, 20 без остатка.
-            if (z > 0 && z % 2 == 0 && z % 5 == 0 && z % 10 == 0 && z % 20 == 0)
+            //Вывод на экран сообщение о том какое z число: положительное или отрицательнлое, четное или нечетное, делиться ли оно на 5, 10, 20 без остатка!
+            if (z > 0)
             {
                 Console.WriteLine("z - положительное число");
+            }
+            else
+            {
+                Console.WriteLine("z - отрицательное число");
+            }
+            if (z %2 == 0)
+            {
+                Console.WriteLine("z - четное число");
+            }
+            else
+            {
+                Console.WriteLine("z - нечетное число");
 
             }
+            if (z % 5 == 0)
+            {
+                Console.WriteLine("z - делиться на 5");
+            }
+            else
+            {
+                Console.WriteLine("z - не делиться на 5");
+
+            }
+            if (z % 10 == 0)
+            {
+                Console.WriteLine("z - делиться на 10");
+            }
+            else
+            {
+                Console.WriteLine("z - не делиться на 10");
+
+            }
+            if (z % 20 == 0)
+            {
+                Console.WriteLine("z - делиться на 20");
+            }
+            else
+            {
+                Console.WriteLine("z - не делиться на 20");
+
+            }
+            Console.ReadLine();
         }
     }
 }
